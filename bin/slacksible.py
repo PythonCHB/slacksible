4cde81ad65a970c0b225b77ad570f7e3ee8a1dd3
Jack Hefner <jmhthethird@gmail.com>
2017-04-23 14:52:03 -0700

    bot: restructure log setup, mark of todos

--- bin/slacksible.py
+++ bin/slacksible.py
@@ -4,20 +4,14 @@ import os
 import sys
 import logging
 
+#just for debug printing, remove later
+import json
 
 from slackclient import SlackClient
 import argparse
 
-# TODOS:
-#   * create logs:
-#       1. Debug log. slacksible_debug.log (10)
-#       2. stderr log. slacksible_stderr.log (40)
-#       3. usage log. slacksible_metrics.log
-#   * create slack bot class:
-#       1. solve token issue (dont show it in code)
-#   * change token to non-test token
 #   * move token out to env var or file and loaded during app boot
-#       Example: `export SLACKSIBLE_TOKEN=xoxb-168959872961-Clds2jLyYvCQY3syhyEUSjKs
+#       Example: export SLACKSIBLE_TOKEN=xoxb-168959872961-Clds2jLyYvCQY3syhyEUSjKs
 
 # Logging levels:
 # CRITICAL    50
@@ -25,6 +19,8 @@ import argparse
 # WARNING     30
 # INFO        20
 # DEBUG       10
+
+
 
 def cli_parser():
     """
@@ -37,7 +33,7 @@ def cli_parser():
     parser.add_argument('--token', '-t', type=str, help="Slack Bot Token generated from Slack platform.")
     return parser.parse_args()
 
-# TODO: review if this can be moved to its own module
+
 def setup_logger(log_name, log_file, level=logging.INFO):
     """
     Easily setup multiple log files and levels
@@ -45,9 +41,7 @@ def setup_logger(log_name, log_file, level=logging.INFO):
     if not os.path.isfile(log_file):
         with open(log_file, 'a'):
             os.utime(log_file, None)
-
-    # formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(module)s - %(message)s')
-    formatter = logging.Formatter("%(asctime)s - [%(threadName)s] [%(levelname)s]  %(message)s")
+    formatter = logging.Formatter("%(asctime)s - [%(module)s] [%(levelname)s]  %(message)s")
     handler = logging.FileHandler(log_file)
     handler.setFormatter(formatter)
     logger = logging.getLogger(log_name)
@@ -60,51 +54,29 @@ class slacksible():
     """
     Ansible slack bot class
     """
-    def __init__(self, token, *args, **kwargs):
-        print("args: ", args)
+    def __init__(self, token, verbose, debug_log, stderr_log, usage_log, *args, **kwargs):
         print("kwargs: ", kwargs)
+        print("verbose: ", verbose)
+        self.verbose = verbose
+        self.debug_log = debug_log
+        self.stderr_log = stderr_log
+        self.usage_log = usage_log
 
-        # TODO: find a better way, this is ugly as hell
-        #       this finds one directory up from where the script is being run in /bin
-        self.log_dir = os.path.split(os.path.abspath(os.path.dirname(sys.argv[0])))[0]+"/log/"
-
-        self.debug_log = setup_logger("slacksible_debug", self.log_dir+"slacksible_debug.log", level=logging.DEBUG)
-        self.debug_log.addHandler(logging.StreamHandler(sys.stdout)) # default for StreamHandler is stderr but we just want to print to console
-        self.stderr_log = setup_logger("slacksible_stderr", self.log_dir+"slacksible_stderr.log", level=logging.ERROR)
-        self.usage_log = setup_logger("slacksible_metrics", self.log_dir+"slacksible_metrics.log", level=logging.INFO)
-
+        # TODO: build config file to read token and bot_name
         if token:
             self.token = token
-            self.debug_log.debug("Token provided on CLI.")
-        else:
+            debug_log.debug("Token provided via CLI.")
+        elif os.environ.get('SLACKSIBLE_TOKEN'):
             self.token = os.environ.get('SLACKSIBLE_TOKEN')
-            self.debug_log.debug("Token retrieved from environment variable")
+            debug_log.debug("Token retrieved from environment variable")
+        else:
+            debug_log.debug("Token not provided")
+            raise NameError("Token not provided")
+        self.bot_name = "ansible_slack_bot"
         self.sc = SlackClient(self.token)
 
-    # TODO: review if this can be moved to its own module
-    def setup_filesystem_dirs(self):
-        '''
-        Creates directory structure for a working application environment
-        No return, makes changes directly on the filesystem
-        '''
-        if not os.path.exists(self.log_path):
-            os.makedirs(self.log_path)
-            self.debug_log.debug("Directory created: ",self.log_path,)
-        else:
-            self.debug_log.debug("Directory exists: ",self.log_path)
 
-    def bot_seppuku(self):
-        '''
-        Restarts running bot application (.py) file
-        '''
-        self.debug_log.debug("Slacksible bot restarting.")
-        os.execv(__file__, sys.argv)
-
-        # App should restart and not get to next line. raise error if it does
-        # TODO: write raise statement to notify that bot did not restart
-        self.stderr_log.error("Slacksible bot restart FAILED.")
-
-    def bot_api_test(self): # simple api test for bot
+    def api_test(self): # simple api test for bot
         '''
         Simple bot API test
         '''
@@ -114,30 +86,30 @@ class slacksible():
         text="Hello from Python! :tada:"
         )
 
-    def bot_listen(self):
-        '''
-        Connect bot to slack api and listen to data stream it has access to
-        '''
-        if self.sc.rtm_connect():
-            self.debug_log.debug("====================Listening====================") # move to debug log
-            while True: # TODO: capture exit from this loop in debug log
-                slack_data = self.sc.rtm_read() # TODO: multi-thread/async this blocking action.
-                if slack_data != [] and "text" in slack_data[0]:
+    def determine_bot_id(self):
+        print(json.dumps(self.sc.api_call("users.list"), sort_keys=False, indent=4))
+        user_list= self.sc.api_call("users.list")
+        #print(user_list["members"])
+        for user in user_list["members"]:
+            print("===========================")
+            print(json.dumps(user, sort_keys=False, indent=4))
+            print(json.dumps(user["name"], sort_keys=False, indent=4))
+            print(json.dumps(user["id"], sort_keys=False, indent=4))
 
-                    # move to debug log
-                    if "message" in slack_data[0]["type"]:
-                        self.debug_log.debug("--------------------------")
-                        self.debug_log.debug(slack_data)
-                        if "user" in slack_data[0]:
-                            self.debug_log.debug("user is:", slack_data[0]["user"])
-                        if "type" in slack_data[0]:
-                            self.debug_log.debug("type is:", slack_data[0]["type"])
-                        if "text" in slack_data[0]:
-                            self.debug_log.debug("message is:", slack_data[0]["text"])
-                        if "channel" in slack_data[0]:
-                            self.debug_log.debug("channel is:", slack_data[0]["channel"])
-        else:
-            self.stderr_log.error("Connection failed to Slack")
+            if user["name"] == self.bot_name:
+                return user["id"]
+
+    def process_response(self, user, channel):
+        # TODO: create command list and route these requests to different functions
+
+        pass
+
+    def seppuku(self):
+        '''
+        Restarts running bot application (.py) file
+        '''
+        self.debug_log.debug("Slacksible bot restarting.")
+        os.execv(__file__, sys.argv)
 
     def query_ARA(self):
         # TODO: create cli parser for reading existing runs
@@ -151,11 +123,42 @@ class slacksible():
         # TODO: capture commands directed at bot and sort by order of usage.
         pass
 
+    def listen(self):
+            '''
+            Connect bot to slack api and listen to data stream it has access to
+            '''
+            if self.sc.rtm_connect():
+                self.debug_log.debug("================= Begin Listening =================")
+                while True:
+                    slack_data = self.sc.rtm_read()
+                    if slack_data != [] and "text" in slack_data[0] and "message" in slack_data[0]["type"]:
+                        # if "message" in slack_data[0]["type"]:
+                        self.debug_log.debug(slack_data)
+                        if "text" in slack_data[0] and slack_data[0]["text"].startswith("<@"+self.bot_id+">"):
+                            self.process_response(slack_data[0]["user"], slack_data[0]["channel"]) # TODO: multi-thread/async this blocking action.")
+            else:
+                self.stderr_log.error("Connection failed to Slack")
+
 
 def main():
     args = cli_parser()
-    bot = slacksible(args.token)
-    bot.bot_listen()
+
+    # TODO: find a better way, this is ugly as hell
+    # this finds one directory up from where the script is being run in /bin and adds the log dir at the end
+    log_dir = os.path.split(os.path.abspath(os.path.dirname(sys.argv[0])))[0]+"/log/"
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    debug_log = setup_logger("slacksible_debug", log_dir+"slacksible_debug.log", level=logging.DEBUG)
+    stderr_log = setup_logger("slacksible_stderr", log_dir+"slacksible_stderr.log", level=logging.ERROR)
+    usage_log = setup_logger("slacksible_metrics", log_dir+"slacksible_metrics.log", level=logging.INFO)
+
+
+    bot = slacksible(args.token, args.verbose, debug_log, stderr_log, usage_log)
+    try:
+        bot.listen()
+    except KeyboardInterrupt:
+        # TODO: move print log when log isn't created yet? Need to restructure log creation.
+        debug_log.debug("Exiting via KeyboardInterrupt")
 
 
 if __name__ == '__main__':
