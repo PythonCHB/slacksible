5f02ade3b4d3c4a9bf34a51231f55bffe818e13f
Jack Hefner <jmhthethird@gmail.com>
2017-05-02 18:39:18 -0700

    bootstrap: initial ara vars

--- setup/bootstrap.py
+++ setup/bootstrap.py
@@ -2,10 +2,23 @@
 
 import os
 import sys
+import logging
 
 
-logging = {}
+"""
+$ export ara_location=$(python -c "import os,ara; print(os.path.dirname(ara.__file__))")
+$ cat > ansible.cfg <<EOF
+[defaults]
+# callback_plugins configuration is required for the ARA callback
+callback_plugins = $ara_location/plugins/callbacks
 
+# action_plugins and library configuration is required for the ara_record and ara_read modules
+action_plugins = $ara_location/plugins/actions
+library = $ara_location/plugins/modules
+EOF
+
+
+"""
 
 def main():
     pass
